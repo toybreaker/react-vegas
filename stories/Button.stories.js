6645a472684911992cb5d384ae7a4d0b86727908
Dracowyn <dracowyn@outlook.com>
2025-03-25 13:39:38 +0800

    init storybook

---
+++ stories/Button.stories.js
@@ -0,0 +1,15 @@
+import { Button } from '../src/Button';
+
+const meta = {
+	title: 'Example/Button',
+	component: Button,
+};
+
+export default meta;
+
+export const Primary = {
+	args: {
+		primary: true,
+		label: 'Button',
+	},
+};
